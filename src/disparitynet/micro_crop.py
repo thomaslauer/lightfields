a8b8acc9eba3f046b3c15f98f83ad3881302e392
Winston Durand <me@winstondurand.com>
2021-05-29 01:28:50 +0000

    update microcrop

--- src/disparitynet/micro_crop.py
+++ src/disparitynet/micro_crop.py
@@ -3,9 +3,10 @@ import glob
 import numpy as np
 import pathlib
 from tqdm import tqdm
+from multiprocessing import Pool
 import utils
 
-INPUT = "../../datasets/flowers_plants/raw/*.png"
+INPUT = "../../datasets/general/raw/*.png"
 OUTFOLDER = "../datasets/flowers_cropped"
 
 def process(img):
@@ -20,4 +21,4 @@ utils.mkdirp(OUTFOLDER)
 if __name__ == '__main__':
     files = glob.glob(INPUT)
     with Pool(4) as p:
-        r = list(tqdm.tqdm(p.imap(process, files), total=len(files)))
+        r = list(tqdm(p.imap(process, files), total=len(files)))
