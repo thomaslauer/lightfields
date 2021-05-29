939f8304d6722a0d2e9069798d8383bf1a80b248
Winston Durand <me@winstondurand.com>
2021-05-29 15:48:45 -0700

    Add stuff

--- src/disparitynet/datasets.py
+++ src/disparitynet/datasets.py
@@ -13,6 +13,7 @@ from pathlib import Path
 
 from concurrent.futures import ProcessPoolExecutor
 
+
 class Processor(object):
     def __init__(self, folder, lightField, grayField, patchSize, stride):
         self.patchSize = patchSize
@@ -32,7 +33,8 @@ class Processor(object):
                 if not os.path.exists(color_file_name):
                     colorPatch = self.lightField[u, v, xStart:xStart+self.patchSize, yStart:yStart+self.patchSize]
                     if colorPatch.shape != (self.patchSize, self.patchSize, 3):
-                        raise Exception(f"incorrect size on color {idx}, {y_i}, {x_i}, {u}, {v}, got {colorPatch.shape}")
+                        raise Exception(
+                            f"incorrect size on color {idx}, {y_i}, {x_i}, {u}, {v}, got {colorPatch.shape}")
                     np.save(color_file_name, np.moveaxis(colorPatch, -1, 0).astype(np.float16))
                 idx += 1
 
@@ -46,7 +48,8 @@ class Processor(object):
                         depth = self.depth(u, v)
                     depthPatch = depth[:, xStart:xStart+self.patchSize, yStart:yStart+self.patchSize]
                     if depthPatch.shape != (200, self.patchSize, self.patchSize):
-                        raise Exception(f"incorrect size on depth {idx}, {y_i}, {x_i}, {u}, {v}, got {depthPatch.shape}")
+                        raise Exception(
+                            f"incorrect size on depth {idx}, {y_i}, {x_i}, {u}, {v}, got {depthPatch.shape}")
                     np.save(depth_file_name, depthPatch.astype(np.float16))
                 idx += 1
 
@@ -54,6 +57,7 @@ class Processor(object):
 
     def depth(self, u, v):
         return preprocess.prepare_depth_features(self.grayField, u / 7, v / 7)
+
 
 class LytroDataset(Dataset):
     STRIDE = 24
@@ -63,9 +67,10 @@ class LytroDataset(Dataset):
     # NOTE: there will be some artefacts ilsn the corners which we want to avoid, so crop the edges
     SIDE_CROP = 60
 
-    def __init__(self, lightFieldPaths: list[str], training=False, cropped=False):
+    def __init__(self, lightFieldPaths: list[str], training=False, cropped=False, workers=8):
         self.training = training
         # NOTE: lightfields stored in 8x8xRxCxRGB
+
         def readImage(path):
             if cropped:
                 return load_extracted(imageio.imread(path))
@@ -89,17 +94,18 @@ class LytroDataset(Dataset):
                     mkdirp(imgDir)
                     print(f"({i}) Loading {path}")
                     rawLightField = readImage(path)
-                    rawLightField = rawLightField[:, :, self.SIDE_CROP:-self.SIDE_CROP, self.SIDE_CROP:-self.SIDE_CROP, :]
+                    rawLightField = rawLightField[:, :, self.SIDE_CROP:-
+                                                  self.SIDE_CROP, self.SIDE_CROP:-self.SIDE_CROP, :]
                     grayField = preprocess.crop_gray(rawLightField)
 
-                    with ProcessPoolExecutor(max_workers=8) as executor:
-                        results = list(tqdm(executor.map(Processor(imgDir, rawLightField, grayField, self.TRAIN, self.STRIDE), coords), total=len(coords)))
+                    with ProcessPoolExecutor(max_workers=workers) as executor:
+                        results = list(tqdm(executor.map(Processor(imgDir, rawLightField, grayField,
+                                       self.TRAIN, self.STRIDE), coords), total=len(coords)))
 
                 for r in range(results[0]):
                     self.patches.append((imgDir, r))
         else:
             self.rawLightFields = [readImage(path) for path in lightFieldPaths]
-
 
     def __len__(self):
         if self.training:
@@ -132,22 +138,23 @@ class LytroDataset(Dataset):
         if self.training:
             # Get the patch disparity
             folder, idx = self.patches[patch]
-            depth = np.load(utils.get_processed_patch_name(folder, idx, targetX, targetY, color=False)).astype(np.float32)
+            depth = np.load(utils.get_processed_patch_name(
+                folder, idx, targetX, targetY, color=False)).astype(np.float32)
 
             colorShape = (1, self.TRAIN - p*2, self.TRAIN - p*2)
 
             color = self.assemble_color_net(
                 colorShape,
-                np.load(utils.get_processed_patch_name(folder, idx, 0, 0, color=True)),
-                np.load(utils.get_processed_patch_name(folder, idx, 0, 7, color=True)),
-                np.load(utils.get_processed_patch_name(folder, idx, 7, 0, color=True)),
-                np.load(utils.get_processed_patch_name(folder, idx, 7, 7, color=True)),
+                self.get_color_patch(patch, 0, 0),
+                self.get_color_patch(patch, 0, 7),
+                self.get_color_patch(patch, 7, 0),
+                self.get_color_patch(patch, 7, 7),
                 u,
                 v,
                 p,
             )
 
-            target = np.load(utils.get_processed_patch_name(folder, idx, targetX, targetY, color=True))[:, p*2:-p*2, p*2:-p*2].astype(np.float32)
+            target = self.get_color_patch(patch, targetX, targetY)[:, p*2:-p*2, p*2:-p*2].astype(np.float32, copy=False)
 
             return depth, color, target
         else:
@@ -173,26 +180,29 @@ class LytroDataset(Dataset):
 
             return depth, color
 
+    def get_color_patch(self, patch, x, y):
+        folder, idx = self.patches[patch]
+        return np.load(utils.get_processed_patch_name(folder, idx, x, y, color=True))
+
     def uv(self, shape, u, v):
         return np.full(shape, u, dtype=np.float32), np.full(shape, v, dtype=np.float32)
 
     def assemble_color_net(self, colorShape, tl, tr, bl, br, u, v, p=6):
         color = np.vstack((
             # Top left (0, 0)
-            tl[:, p:-p, p:-p].astype(np.float32),
+            tl[:, p:-p, p:-p].astype(np.float32, copy=False),
             *self.uv(colorShape, 0, 0),
             # Top right (0, 1)
-            tr[:, p:-p, p:-p].astype(np.float32),
+            tr[:, p:-p, p:-p].astype(np.float32, copy=False),
             *self.uv(colorShape, 0, 1),
             # Bottom left (1, 0)
-            bl[:, p:-p, p:-p].astype(np.float32),
+            bl[:, p:-p, p:-p].astype(np.float32, copy=False),
             *self.uv(colorShape, 1, 0),
             # Bottom right (1, 1)
-            br[:, p:-p, p:-p].astype(np.float32),
+            br[:, p:-p, p:-p].astype(np.float32, copy=False),
             *self.uv(colorShape, 1, 1),
             # U', V'
             np.full(colorShape, u, dtype=np.float32),
             np.full(colorShape, v, dtype=np.float32)
         ))
         return color
-
--- src/disparitynet/main.py
+++ src/disparitynet/main.py
@@ -9,6 +9,7 @@ import datasets
 import networks
 import params
 import utils
+import imageio
 
 
 def train(net, train_loader, optimizer, device, epoch):
@@ -23,8 +24,8 @@ def train(net, train_loader, optimizer, device, epoch):
         color = color.to(device)
         target = target.to(device)
 
-        images = color[:,:-2,:,:]
-        novelLocation = color[:,-2:,:,:]
+        images = color[:, :-2, :, :]
+        novelLocation = color[:, -2:, :, :]
 
         predicted = net(depth, images, novelLocation)
 
@@ -32,9 +33,11 @@ def train(net, train_loader, optimizer, device, epoch):
         loss.backward()
         optimizer.step()
 
+
 def saveModel(net, epoch):
     utils.mkdirp("./checkpoints")
     torch.save(net.state_dict(), utils.get_checkpoint_path(epoch))
+
 
 def validate(net, validation_loader, device, epoch):
 
@@ -49,8 +52,8 @@ def validate(net, validation_loader, device, epoch):
             color = color.to(device)
             target = target.to(device)
 
-            images = color[:,:-2,:,:]
-            novelLocation = color[:,-2:,:,:]
+            images = color[:, :-2, :, :]
+            novelLocation = color[:, -2:, :, :]
 
             predicted = net(depth, images, novelLocation)
             loss = F.mse_loss(predicted, target)
@@ -61,6 +64,20 @@ def validate(net, validation_loader, device, epoch):
 
     return loss_sum
 
+
+def test_image(net, depth, color, device, epoch, out_folder="./eval_test"):
+    mkdirp(out_folder)
+    net.eval()
+    with torch.no_grad():
+        depth = torch.unsqueeze(torch.Tensor(depth), 0).to(device)
+        color = torch.unsqueeze(torch.Tensor(color), 0).to(device)
+
+        images = color[:, :-2, :, :]
+        novelLocation = color[:, -2:, :, :]
+
+        output = net(depth, images, novelLocation).cpu()
+        img = utils.torch2np_color(output[0].detach().numpy() ** (1/2.2))
+        imageio.imwrite(f"{out_folder}/epoch_{epoch:03}.png", img)
 
 
 def main():
@@ -74,8 +91,18 @@ def main():
     #     "../datasets/reflective_18_eslf.png",
     # ]
 
+    print("Building training data")
     lightFieldPaths = glob.glob(f'{params.drive_path}/datasets/microcropped_images/*.png')
     full_dataset = datasets.LytroDataset(lightFieldPaths, training=True, cropped=True)
+    flower_9 = f'{params.drive_path}/datasets/microcropped_images/flowers_plants_9_eslf.png'
+
+    print("Building test data")
+    # Fetch test data
+    test_data = datasets.LytroDataset(flower_9, training=False, cropped=True)
+    test_depth, test_color = test_data[36]
+    # clear memory if possible...
+    test_data = None
+    print("Splitting train/validate")
 
     train_size = int(0.9 * len(full_dataset))
     validate_size = len(full_dataset) - train_size
@@ -88,7 +115,6 @@ def main():
     workers = 4
 
     train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers=workers)
-
     validate_loader = DataLoader(validate_dataset, shuffle=True, batch_size=batch_size, num_workers=workers)
 
     net = networks.FullNet(device)
@@ -100,15 +126,16 @@ def main():
     # move net to cuda BEFORE setting optimizer variables
     net = net.to(device)
     optimizer = torch.optim.SGD(net.parameters(), lr=params.sgd_lr, momentum=params.sgd_momentum)
-
+    if params.start_epoch != 0:
+        test_image(net, test_depth, test_color, device, params.start_epoch, out_folder="./eval_test")
 
     for epoch in range(params.start_epoch, params.start_epoch + params.epochs):
         train(net, train_loader, optimizer, device, epoch)
         saveModel(net, epoch)
         loss = validate(net, validate_loader, device, epoch)
         print(f"Validation loss is {loss}")
-
-
+        test_image(net, test_depth, test_color, device, epoch, out_folder="./eval_test")
+        print(f"Saved image for epoch {epoch}")
 
 
 if __name__ == "__main__":
