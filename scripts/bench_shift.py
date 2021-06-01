Fix scaling in eval

scripts/bench_shift.py
@@ -60,15 +60,15 @@ def compare(name, img1, img2):
 
 
 def main():
-    source = "../tmp/flowers_25.png"
+    source = "../../tmp/flowers_25.png"
     img = cv.cvtColor(cv.imread(source, cv.IMREAD_COLOR)[
-                      7::14, 7::14].astype(np.float32) / 255, cv.COLOR_BGR2GRAY)
+                      7::14, 7::14].astype(np.float32) / 255, cv.COLOR_BGR2GRAY)[:102, :102]
     print(img.shape)
     result_shift = np.empty(img.shape, dtype=np.float32)
     result_shift_filter = np.empty(img.shape, dtype=np.float32)
     result_affine = np.empty(img.shape, dtype=np.float32)
 
-    count = 20
+    count = 100
     u, v = -21, 10.98
 
     shifts = []

src/disparitynet/eval.py
@@ -21,11 +21,11 @@ def main():
         # "../datasets/reflective_17_eslf.png",
         # "../datasets/reflective_18_eslf.png",
         # "../datasets/flowers_plants/raw/flowers_plants_25_eslf.png"
-        # "../datasets/microcropped_images/flowers_plants_25_eslf.png"
-        f"{params.drive_path}/datasets/microcropped_images/flowers_plants_9_eslf.png"
+        "../datasets/microcropped_images/Rock.png"
+        # f"{params.drive_path}/datasets/microcropped_images/flowers_plants_9_eslf.png"
     ]
 
-    epochNum = 4
+    epochNum = 12
 
     full_dataset = datasets.LytroDataset(lightFieldPaths, training=False, cropped=True)
 
@@ -34,6 +34,9 @@ def main():
 
     net = networks.FullNet(device)
     net.load_state_dict(torch.load(utils.get_checkpoint_path(epochNum), map_location=torch.device(device)))
+    # net = torch.jit.trace(net, (torch.rand(1, 200, 376, 541), torch.rand(
+    #     1, 4 * 5, 376 - 12, 541 - 12), torch.rand(1, 2, 376 - 12, 541 - 12)))
+    # torch.jit.save(net, "backups/main_model.pt")
     net = net.to(device)
     net.eval()
 
@@ -56,9 +59,9 @@ def main():
         utils.mkdirp(f"output/epoch_{epochNum}/color")
         utils.mkdirp(f"output/epoch_{epochNum}/disp")
         utils.mkdirp(f"output/epoch_{epochNum}/raw")
-        imageio.imwrite(f"output/epoch_{epochNum}/color/nn_0{y}_0{x}.png", img)
+        imageio.imwrite(f"output/epoch_{epochNum}/color/nn_0{y}_0{x}.png", np.clip(img * 255, 0, 255).astype(np.uint8))
         imageio.imwrite(f"output/epoch_{epochNum}/disp/nn_0{y}_0{x}.png", dispImg)
-        imageio.imwrite(f"output/epoch_{epochNum}/raw/nn_0{y}_0{x}.png", rawImg)
+        imageio.imwrite(f"output/epoch_{epochNum}/raw/nn_0{y}_0{x}.png", np.clip(rawImg * 255, 0, 255).astype(np.uint8))
 
     return
 

src/disparitynet/networks.py
@@ -95,7 +95,8 @@ class FullNet(nn.Module):
             # p_i = p_i[:, :rows, :cols, :]
 
             projectedLocations = grid + (p_i - reshapedNovelLocation) * dupedDisparity
-            projectedLocations = projectedLocations / torch.tensor([[[[rows-1, cols-1]]]], device=self.device)
+            projectedLocations[..., 0] /= rows - 1
+            projectedLocations[..., 1] /= cols - 1
             projectedLocations = (projectedLocations - 0.5) * 2
             # print(projectedLocations.requires_grad)
 